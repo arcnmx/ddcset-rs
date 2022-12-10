eedf754453433dbc665660a54168ebe9efa46fcb
arcnmx <arcnmx@users.noreply.github.com>
2022-12-10 00:35:29 -0800

    feat: update to winapi EDID rewrite

--- lock.nix
+++ lock.nix
@@ -1,10 +1,10 @@
 {
   outputHashes = {
     "ddc-0.3.0" = "sha256-n36HNhYbcu1FbvzcyArdwdsJj0yiXBuu9a/Zz91v30o=";
-    "ddc-hi-0.5.0" = "sha256-eFgL+AT2QHxFE9KDEar06E4gWaEzEaycwp1sw2SesM4=";
+    "ddc-hi-0.5.0" = "sha256-j1J0mLMqeUWI/KTNl8AFjyOgjICS/e8h+EYpvAuwg/A=";
     "ddc-i2c-0.3.0" = "sha256-uzOzEfADIRmpnV1EKeWhDcoXFyv6jtbiBdl+y2LAMFA=";
     "ddc-macos-0.3.0" = "sha256-O8OQKke4fGvBbM3pvc/6/8d4IkebbMaVKkbaPFVe4I8=";
-    "ddc-winapi-0.3.0" = "sha256-+G4PDuWBfgofuaPCOzRTmGgip5liWkBib6JHlW+mZxY=";
+    "ddc-winapi-0.3.0" = "sha256-8Rk65QAUQso6riAQJ8b0vRRv2MucccBlR4dXMLoejic=";
     "i2c-linux-0.2.0" = "sha256-mBEZbbRW9csQ2t/m2n0AMQGlf67252y2+cT3OHE6P7c=";
     "mccs-0.2.0" = "sha256-HULGP1ciDbknV7KdBvm5Sa1SoMf3jKyt1lyUNusjITQ=";
     "mccs-caps-0.2.0" = "sha256-HULGP1ciDbknV7KdBvm5Sa1SoMf3jKyt1lyUNusjITQ=";
